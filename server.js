8b1a2dd75227b9a024050babb26d6f0891e9cb32
b-blots-tran <brian.tran0121@gmail.com>
2023-10-03 16:29:20 -0400

    initial commit with default webpack

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const webpack = requrie("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+
+const app = express();
+const config = require("./webpack.config.js");
+const compiler = webpack(config);
+
+const PORT = 3000;
+app.use(webpackDevMiddleware(compiler, {publicPath:config.output.publicPath});
+
+app.listen(PORT,function(){
+    console.log(`App listening on port ${PORT}`)
+})
